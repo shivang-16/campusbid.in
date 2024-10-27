eb7a5b78c4c588f8dd707716eb688636549cadfd
piyush0049 <piyushjoshi81204@gmail.com>
2024-10-28 00:05:49 +0530

    update the favicon and main logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ import GreenButton from '@/components/GreenButton';
 const Navbar: React.FC = () => {
     return (
         <header className="w-full px-2 lg:px-6 flex justify-between items-center backdrop-blur-3xl shadow-md fixed top-0 left-0 z-50">
-            <div className="flex items-center space-x-2">
-                <img src="/campusbid_logo.ico" alt="Logo" className="w-16 h-16" />
+            <div className="flex items-center space-x-2 py-2">
+                <img src="/campusbid_logo.ico" alt="Logo" className="w-12 h-12" />
                 <span className="text-xl font-extrabold text-teal-600">CampusBid</span>
             </div>
             <nav className="hidden md:flex space-x-6 text-gray-700 font-medium">
