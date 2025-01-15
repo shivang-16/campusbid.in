2719dc67e050015d6edc83a3a0fd7bdee1117702
piyush0049 <piyushjoshi81204@gmail.com>
2025-01-15 16:36:25 +0530

    responsive 3

--- src/app/(root)/(dashboard)/_components/timeline.tsx
+++ src/app/(root)/(dashboard)/_components/timeline.tsx
@@ -197,7 +197,7 @@ export function HeroHighlightDemo() {
     ];
 
     return (
-        <HeroHighlight>
+        <HeroHighlight containerClassName="group">
             <motion.h1
                 initial={{
                     opacity: 0,
@@ -211,7 +211,7 @@ export function HeroHighlightDemo() {
                     duration: 0.5,
                     ease: [0.4, 0.0, 0.2, 1],
                 }}
-                className="text-2xl  md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white w-full leading-relaxed lg:leading-snug text-center mx-auto "
+                className="text-2xl md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white w-full leading-relaxed lg:leading-snug text-center mx-auto"
             >
                 <div className="relative w-full">
                     <Timeline data={data} />
@@ -225,4 +225,5 @@ export function HeroHighlightDemo() {
             </motion.h1>
         </HeroHighlight>
     );
+    
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
       <footer className="w-full py-8 px-8 lg:px-16 z-50">
         <div className="flex flex-col items-center space-y-4">
-          <h4 className="text-teal-800 font-bold text-xl md:text-2xl">
+          <h4 className="text-teal-700 font-bold text-xl md:text-2xl">
             Follow Us:
           </h4>
           <div className="flex space-x-6 mb-4">
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
               href="https://x.com/campus_bid"
               target="_blank"
               aria-label="Twitter"
-              className="text-teal-800 hover:text-teal-900 transition duration-300"
+              className="text-teal-700 hover:text-teal-900 transition duration-300"
             >
               <FaXTwitter className="w-6 h-6" />
             </a>
@@ -23,7 +23,7 @@ const Footer: React.FC = () => {
               href="https://www.linkedin.com/company/campusbid/"
               target="_blank"
               aria-label="LinkedIn"
-              className="text-teal-800 hover:text-teal-900 transition duration-300"
+              className="text-teal-700 hover:text-teal-900 transition duration-300"
             >
               <FaLinkedinIn className="w-6 h-6" />
             </a>
@@ -31,12 +31,12 @@ const Footer: React.FC = () => {
               href="https://www.instagram.com/campusbid.in/"
               target="_blank"
               aria-label="Instagram"
-              className="text-teal-800 hover:text-teal-900 transition duration-300"
+              className="text-teal-700 hover:text-teal-900 transition duration-300"
             >
               <FaInstagram className="w-6 h-6" />
             </a>
           </div>
-          <div className="text-xs md:text-sm text-gray-600 mb-2">
+          <div className="text-xs md:text-sm text-gray-400 mb-2">
           All rights reserved to CampusBid, Inc.
         </div>
         </div>
